8d544dbf25a473c5417bc82fa4e69a0a21563429
diwakar419 <89566125+diwakar419@users.noreply.github.com>
2021-08-31 10:47:31 +0530

    Update vars.tf

--- cfntemplates/vars.tf
+++ cfntemplates/vars.tf
@@ -1,2 +1,11 @@
-variable "AWS_ACCESS_KEY"{}
-variable "AWS_SECRET_KEY"{} 
+variable "aws_access_key" {
+  type        = string
+  description = "AWS Access key"
+  sensitive = true
+}
+
+variable "aws_secret_key" {
+  type        = string
+  description = "AWS Secret key"
+  sensitive = true
+}
