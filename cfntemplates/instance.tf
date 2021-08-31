5fb1f7bc70336b5cfd31f306ea5a36ed38847c5b
diwakar419 <89566125+diwakar419@users.noreply.github.com>
2021-08-31 11:22:12 +0530

    Update instance.tf

--- cfntemplates/instance.tf
+++ cfntemplates/instance.tf
@@ -1,13 +1,12 @@
 provider "aws" {
   region = "us-east-1"
-  access_key=var.aws_access_key
-  secret_key=var.aws_secret_key 
+  access_key=var.AWS_ACCESS_KEY
+  secret_key=var.AWS_SECRET_ACCESS_KEY 
 }
 resource "aws_instance" "web" {
   ami           = "ami-02dc8ad50da58fffd"
   instance_type = "t3.micro"
 
   tags = {
-    Name = "Diwakar_TF"
+    Name = "HelloWorld"
   }
-}
